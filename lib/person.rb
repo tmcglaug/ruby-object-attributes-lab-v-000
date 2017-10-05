a8e49aa017ba52f74f78105e017bcb35778c8935
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-05 02:49:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,16 @@
 class Person
 
-  def initialize(name, job)
-  @name = name
-  @job = job
-
+  def name
+@name = name
   end
 
 def name=(name)
 @name = name
+end
+
+def job
+@job = job
+
 end
 
 def job=(job)
